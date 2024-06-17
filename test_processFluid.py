0b8054385dbb409233e45fac393225f88a6cd8f4
Jake <63001043+Jakeabeast@users.noreply.github.com>
2024-06-17 12:18:45 +1000

    update test again

--- test_processFluid.py
+++ test_processFluid.py
@@ -4,10 +4,10 @@ from processFluid import process
 
 #add multiple test parameters for unit test
 @pytest.mark.parametrize("args, expected", [
-    (*["Water", "Blood", "Plasma"], True),
-    (*["Water", "Plasma"], False),
+    (["Water", "Blood", "Plasma"], True),
+    (["Water", "Plasma"], False),
 ])
 
 def test_process(args, expected):
-    print(args)
-    assert process(args) == expected
+    print(*args)
+    assert process(*args) == expected
