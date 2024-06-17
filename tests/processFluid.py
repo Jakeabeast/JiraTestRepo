c7f8d4140df13f740ca96f9c86a0b068843d4104
Jake <63001043+Jakeabeast@users.noreply.github.com>
2024-06-17 11:56:39 +1000

    added unit test using pytests

---
+++ tests/processFluid.py
@@ -0,0 +1,13 @@
+import pytest
+
+from processFluid import process
+
+#add multiple test parameters for unit test
+@pytest.mark.parametrize("args, expected", [
+    (["Water", "Blood", "Plasma"], True),
+    (["Water"], False),
+])
+
+def test_process(args, expected):
+    print(args)
+    assert process(args) == expected
