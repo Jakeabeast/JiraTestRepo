eb666be69d230ef504e1e53eed660b68f58f734f
Jake <63001043+Jakeabeast@users.noreply.github.com>
2024-06-17 12:33:42 +1000

    tests can view functions in parent folder now

--- tests/test_processFluid.py
+++ tests/test_processFluid.py
@@ -1,6 +1,13 @@
 import pytest
+import sys
 
+#make all files in application folder viewable for test subdirectory
+sys.path.append('./')
+
+#import necessary functions here
 from processFluid import process
+
+#-----------------------------------------------------------
 
 #add multiple test parameters for unit test
 @pytest.mark.parametrize("args, expected", [
