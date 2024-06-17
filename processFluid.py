ae791590c47ab9776da4b9d862bdb60f7768b754
Jake <63001043+Jakeabeast@users.noreply.github.com>
2024-06-17 11:56:25 +1000

    return appropriate boolean for function outcome

--- processFluid.py
+++ processFluid.py
@@ -1,8 +1,10 @@
 def process(*parameter):
     if len(parameter) > 2:
         print(parameter)
+        return True
     else:
         print("not enough to process")
+        return False
 
 def results():
     print("working")
